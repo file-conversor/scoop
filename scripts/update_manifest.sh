195aa9a81515546d261d56dac15c260bca2783ce
Andre <1559470+andre-romano@users.noreply.github.com>
2026-05-02 17:19:15 -0300

    moved env vars to git actions (SSOT layout)

--- scripts/update_manifest.sh
+++ scripts/update_manifest.sh
@@ -10,25 +10,21 @@ trap 'echo "Error occurred at line $LINENO while executing: $BASH_COMMAND"' ERR
 
 VERSION_SEMVER=$(echo "$VERSION" | sed 's/^v//') # Remove leading 'v' if present
 
-OUTPUT_BUCKET="bucket/file_conversor.json"
+OUTPUT_BUCKET="bucket/${APP_NAME}.json"
 
 INSTALLER_NAME="file_conversor-${VERSION}-Win_x64-Installer.exe"
-CHECKSUMS_FILE="checksum.sha256"
-
-DESCRIPTION="Universal file conversor for multimedia files (audio, video, pdf, docs, etc)."
-LICENSE="Apache-2.0"
-HOMEPAGE="https://github.com/file-conversor/file_conversor"
 RELEASE_URL="${HOMEPAGE}/releases/download/${VERSION}"
 
 echo "Generating manifest ..."
 echo
 echo "  ENV: "
+echo "    APP_NAME: ${APP_NAME}"
 echo "    VERSION: ${VERSION}"
-echo "    INSTALLER_NAME: ${INSTALLER_NAME}"
-echo "    CHECKSUMS_FILE: ${CHECKSUMS_FILE}"
 echo "    DESCRIPTION: ${DESCRIPTION}"
 echo "    LICENSE: ${LICENSE}"
 echo "    HOMEPAGE: ${HOMEPAGE}"
+echo "    CHECKSUMS_FILE: ${CHECKSUMS_FILE}"
+echo "    INSTALLER_NAME: ${INSTALLER_NAME}"
 echo 
 
 echo "  1. Downloading checksums ..."
@@ -46,6 +42,9 @@ cat > "$OUTPUT_BUCKET" <<EOL
     "license": "${LICENSE}",
     "url": "${RELEASE_URL}/${INSTALLER_NAME}",
     "hash": "${CHECKSUM_HASH}",    
+    "bin": [
+        "${APP_NAME}.bat"
+    ],
     "pre_install": [
         "\$exePath = Get-ChildItem -Path \"\$dir\" -Filter *-Installer.exe  | Select-Object -ExpandProperty FullName",
         "Start-Process -FilePath \"\$exePath\" -ArgumentList \"/DIR=\$dir\", \"/CURRENTUSER\", \"/SUPPRESSMSGBOXES\", \"/VERYSILENT\", \"/NORESTART\", \"/SP-\" -Wait",
